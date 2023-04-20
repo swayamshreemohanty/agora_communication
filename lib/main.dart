9db9fec8d9068bad7bd8d56c719864046e47e86f
Swayamshree Mohanty <swayamshree.appdev@colourmoon.com>
2023-04-20 13:32:11 +0530

    text field moved to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:agora_example_test/communication/logic/av_controller/av_controll
 import 'package:agora_example_test/communication/model/agora_creds_model.dart';
 import 'package:agora_example_test/communication/screens/video_communication_screen.dart';
 import 'package:agora_example_test/communication/screens/voice_communication_screen.dart';
-import 'package:agora_example_test/communication/widget/chat/agora_credential_dialog_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -21,6 +20,10 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // Create UI with local view and remote view
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  final appIdTextController = TextEditingController();
+  final tokenTextController = TextEditingController();
+  final channelIdTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,109 +31,175 @@ class _MyAppState extends State<MyApp> {
         title: const Text('Communication'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                final agoraCreds = await showDialog<AgoraCredentialsModel>(
-                  context: context,
-                  builder: (context) {
-                    return const AlertDialog(
-                      content: EnterCredentialDialogWidget(),
-                    );
-                  },
-                );
-                if (agoraCreds != null) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MultiBlocProvider(
-                        providers: [
-                          BlocProvider(
-                            create: (context) => AgoraAvManagerCubit(),
-                          ),
-                          BlocProvider(
-                            create: (context) => AvControllerCubit(
-                              agoraAvManagerCubit:
-                                  context.read<AgoraAvManagerCubit>(),
-                            ),
-                          ),
-                        ],
-                        child: VideoCommunicationScreen(
-                            agoraCredentialsModel: agoraCreds),
+        child: Form(
+          key: formkey,
+          child: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(15),
+                padding: const EdgeInsets.only(bottom: 20, left: 10, right: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: appIdTextController,
+                      decoration: const InputDecoration(
+                        hintText: "Enter App Id",
+                        labelText: "Enter App Id",
                       ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Field can't be empty";
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
-                  );
-                } else {
-                  Fluttertoast.showToast(
-                    msg: "No Credentials Found",
-                    backgroundColor: Colors.red,
-                    gravity: ToastGravity.CENTER,
-                  );
-                }
-              },
-              child: const Text("Video call"),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final agoraCreds = await showDialog<AgoraCredentialsModel>(
-                  context: context,
-                  builder: (context) {
-                    return const AlertDialog(
-                      content: EnterCredentialDialogWidget(),
-                    );
-                  },
-                );
-                if (agoraCreds != null) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MultiBlocProvider(
-                        providers: [
-                          BlocProvider(
-                            create: (context) => AgoraAvManagerCubit(),
-                          ),
-                          BlocProvider(
-                            create: (context) => AvControllerCubit(
-                              agoraAvManagerCubit:
-                                  context.read<AgoraAvManagerCubit>(),
-                            ),
-                          ),
-                        ],
-                        child: VoiceCommunicationScreen(
-                            agoraCredentialsModel: agoraCreds),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: channelIdTextController,
+                      decoration: const InputDecoration(
+                        hintText: "Enter Channel Name",
+                        labelText: "Enter Channel Name",
                       ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Field can't be empty";
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
-                  );
-                } else {
-                  Fluttertoast.showToast(
-                    msg: "No Credentials Found",
-                    backgroundColor: Colors.red,
-                    gravity: ToastGravity.CENTER,
-                  );
-                }
-              },
-              child: const Text("Voice call"),
-            ),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     Navigator.push(
-            //       context,
-            //       MaterialPageRoute(
-            //         builder: (context) => BlocProvider(
-            //           create: (context) => AgoraChatManagerCubit(),
-            //           child: const ChatScreen(),
-            //         ),
-            //       ),
-            //     );
-            //   },
-            //   child: const Text("Chat"),
-            // )
-          ],
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: tokenTextController,
+                      decoration: const InputDecoration(
+                        hintText: "Enter Token",
+                        labelText: "Enter Token",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Field can't be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (formkey.currentState!.validate()) {
+                          FocusScope.of(context).unfocus();
+                          final agoraCreds = AgoraCredentialsModel(
+                            appId: appIdTextController.text.trim(),
+                            channelName: channelIdTextController.text.trim(),
+                            token: tokenTextController.text.trim(),
+                          );
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MultiBlocProvider(
+                                providers: [
+                                  BlocProvider(
+                                    create: (context) => AgoraAvManagerCubit(),
+                                  ),
+                                  BlocProvider(
+                                    create: (context) => AvControllerCubit(
+                                      agoraAvManagerCubit:
+                                          context.read<AgoraAvManagerCubit>(),
+                                    ),
+                                  ),
+                                ],
+                                child: VideoCommunicationScreen(
+                                  agoraCredentialsModel: agoraCreds,
+                                ),
+                              ),
+                            ),
+                          );
+                        } else {
+                          Fluttertoast.showToast(
+                            msg: "No Credentials Found",
+                            backgroundColor: Colors.red,
+                            gravity: ToastGravity.CENTER,
+                          );
+                        }
+                      },
+                      child: const Text("Start Video call"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (formkey.currentState!.validate()) {
+                          FocusScope.of(context).unfocus();
+
+                          final agoraCreds = AgoraCredentialsModel(
+                            appId: appIdTextController.text.trim(),
+                            channelName: channelIdTextController.text.trim(),
+                            token: tokenTextController.text.trim(),
+                          );
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MultiBlocProvider(
+                                providers: [
+                                  BlocProvider(
+                                    create: (context) => AgoraAvManagerCubit(),
+                                  ),
+                                  BlocProvider(
+                                    create: (context) => AvControllerCubit(
+                                      agoraAvManagerCubit:
+                                          context.read<AgoraAvManagerCubit>(),
+                                    ),
+                                  ),
+                                ],
+                                child: VoiceCommunicationScreen(
+                                    agoraCredentialsModel: agoraCreds),
+                              ),
+                            ),
+                          );
+                        } else {
+                          Fluttertoast.showToast(
+                            msg: "No Credentials Found",
+                            backgroundColor: Colors.red,
+                            gravity: ToastGravity.CENTER,
+                          );
+                        }
+                      },
+                      child: const Text("Start Voice call"),
+                    ),
+                  ],
+                ),
+              ),
+
+              // ElevatedButton(
+              //   onPressed: () {
+              //     Navigator.push(
+              //       context,
+              //       MaterialPageRoute(
+              //         builder: (context) => BlocProvider(
+              //           create: (context) => AgoraChatManagerCubit(),
+              //           child: const ChatScreen(),
+              //         ),
+              //       ),
+              //     );
+              //   },
+              //   child: const Text("Chat"),
+              // )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
